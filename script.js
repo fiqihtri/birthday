eb04e130339a8fceeb22418306f684cf54305c72
Fiqih Tri Eko <109412485+fiqihtri@users.noreply.github.com>
2022-08-02 17:06:21 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Ketik Nama Lengkapmu");
+ var b=prompt("Ketik Bulan Kelahiranmu");
+  	if(a== "Anindya Lintang Kirana" && b=="Agustus")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("Salah ketik kirana. Coba lagi..");
+  		window.location.href="index.html";
+  	}
+  	
